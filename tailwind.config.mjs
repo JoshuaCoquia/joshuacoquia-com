884d77133e0496ecf80c3c4ae04c29d67a48936a
Joshua Coquia <joshua.coquia@hotmail.com>
2024-07-20 13:41:18 -0700

    add colors and font sizes to site

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			text: '#090d0f',
+			background: '#f7fafb',
+			transparent: 'transparent',
+			current: 'currentColor',
+			primary: '#8cbfd9',
+			secondary: '#193b4d',
+			accent: '#702424',
+		},
 		extend: {},
+		fontSize: {
+			base: '1rem',
+			xl: '1.5rem',
+			'2xl': '2rem',
+			'3xl': '2.5rem',
+			'4xl': '4.5rem',
+		}
 	},
 	plugins: [],
 }
